3fc1e94098e324c84c13d3fe613f89286cb72935
pauldambra <paul.dambra@gmail.com>
2013-04-18 16:14:14 +0100

    adding bezier view to project

---
+++ src/org/sleepyparents/littleletters/views/BezierCurveWritingView.java
@@ -0,0 +1,168 @@
+package org.sleepyparents.littleletters.views;
+
+import no.geosoft.cc.geometry.spline.SplineFactory;
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.RectF;
+import android.graphics.drawable.BitmapDrawable;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class BezierCurveWritingView extends View {
+	private static final float STROKE_WIDTH = 5f;
+
+	  /** Need to track this so the dirty region can accommodate the stroke. **/
+	  private static final float HALF_STROKE_WIDTH = STROKE_WIDTH / 2;
+
+	  private Paint paint = new Paint();
+	  private Path path = new Path();
+
+	  /**
+	   * Optimizes painting by invalidating the smallest possible area.
+	   */
+	  private float lastTouchX;
+	  private float lastTouchY;
+	  private final RectF dirtyRect = new RectF();
+
+	private Canvas lastCanvas;
+
+	  public BezierCurveWritingView(Context context, AttributeSet attrs) {
+	    super(context, attrs);
+
+	    paint.setAntiAlias(true);
+	    paint.setColor(Color.BLACK);
+	    paint.setStyle(Paint.Style.STROKE);
+	    paint.setStrokeJoin(Paint.Join.ROUND);
+	    paint.setStrokeWidth(STROKE_WIDTH);
+	  }
+
+	  public Bitmap getCanvasAsBitmap(Resources res) {
+		  BitmapDrawable bitmapDrawable = new BitmapDrawable(res);
+		  bitmapDrawable.draw(lastCanvas);
+		  return bitmapDrawable.getBitmap();
+	  }
+	  
+	  /**
+	   * Erases the signature.
+	   */
+	  public void clear() {
+	    path.reset();
+
+	    // Repaints the entire view.
+	    invalidate();
+	  }
+
+	  @Override
+	  protected void onDraw(Canvas canvas) {
+	    canvas.drawPath(path, paint);
+		lastCanvas = canvas;    
+	  }
+
+	  @Override
+	  public boolean onTouchEvent(MotionEvent event) {
+	    float eventX = event.getX();
+	    float eventY = event.getY();
+
+	    switch (event.getAction()) {
+	      case MotionEvent.ACTION_DOWN:
+	        path.moveTo(eventX, eventY);
+	        lastTouchX = eventX;
+	        lastTouchY = eventY;
+	        // There is no end point yet, so don't waste cycles invalidating.
+	        return true;
+
+	      case MotionEvent.ACTION_MOVE:
+	      case MotionEvent.ACTION_UP:
+	        // Start tracking the dirty region.
+	        resetDirtyRect(eventX, eventY);
+	        
+	        double lastTouchPointX = lastTouchX;
+	        double lastTouchPointY = lastTouchY;
+	        // When the hardware tracks events faster than they are delivered, the
+	        // event will contain a history of those skipped points.
+	        int historySize = event.getHistorySize();
+	        for (int i = 0; i < historySize; i++) {
+	          float historicalX = event.getHistoricalX(i);
+	          float historicalY = event.getHistoricalY(i);
+	          expandDirtyRect(historicalX, historicalY);
+	          getCubicBezierPath(lastTouchPointX, lastTouchPointY, historicalX,
+					historicalY);
+	          lastTouchPointX = historicalX;
+	          lastTouchPointY = historicalY;
+	        }
+
+	        // After replaying history, connect the line to the touch point.
+	        getCubicBezierPath(lastTouchPointX, lastTouchPointY, eventX, eventY);
+	        break;
+
+	      default:
+	        Log.d(this.getClass().getName(),"Ignored touch event: " + event.toString());
+	        return false;
+	    }
+
+	    // Include half the stroke width to avoid clipping.
+	    invalidate(
+	        (int) (dirtyRect.left - HALF_STROKE_WIDTH),
+	        (int) (dirtyRect.top - HALF_STROKE_WIDTH),
+	        (int) (dirtyRect.right + HALF_STROKE_WIDTH),
+	        (int) (dirtyRect.bottom + HALF_STROKE_WIDTH));
+	    
+	    lastTouchX = eventX;
+	    lastTouchY = eventY;
+
+	    return true;
+	  }
+
+	private void getCubicBezierPath(double startX,
+			double startY, float endX, float endY) {
+		double[] touchPoints = new double[6];
+		  touchPoints[0] = startX;
+		  touchPoints[1] = startY;
+		  touchPoints[2] = 0.0;
+		  touchPoints[3] = endX;
+		  touchPoints[4] = endY;
+		  touchPoints[5] = 0.0;
+		  double[] splineResult = SplineFactory.createCatmullRom(touchPoints, 3);
+		  //path.lineTo(historicalX, historicalY);
+		  path.cubicTo((float) splineResult[3], (float) splineResult[4], (float) splineResult[6], (float) splineResult[7], endX, endY);
+	}
+
+	  /**
+	   * Called when replaying history to ensure the dirty region includes all
+	   * points.
+	   */
+	  private void expandDirtyRect(float historicalX, float historicalY) {
+	    if (historicalX < dirtyRect.left) {
+	      dirtyRect.left = historicalX;
+	    } else if (historicalX > dirtyRect.right) {
+	      dirtyRect.right = historicalX;
+	    }
+	    if (historicalY < dirtyRect.top) {
+	      dirtyRect.top = historicalY;
+	    } else if (historicalY > dirtyRect.bottom) {
+	      dirtyRect.bottom = historicalY;
+	    }
+	  }
+
+	  /**
+	   * Resets the dirty region when the motion event occurs.
+	   */
+	  private void resetDirtyRect(float eventX, float eventY) {
+
+	    // The lastTouchX and lastTouchY were set when the ACTION_DOWN
+	    // motion event occurred.
+	    dirtyRect.left = Math.min(lastTouchX, eventX);
+	    dirtyRect.right = Math.max(lastTouchX, eventX);
+	    dirtyRect.top = Math.min(lastTouchY, eventY);
+	    dirtyRect.bottom = Math.max(lastTouchY, eventY);
+	  }
+	  
+	  
+}
